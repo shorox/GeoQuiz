1bcfdfb47b86ef4b6c525cabc3c7566db3e89763
Merzkiy <skazo4nix@gmail.com>
2016-05-25 16:54:18 +0300

    final

--- app/src/main/java/com/spark/geoquiz/QuizActivity.java
+++ app/src/main/java/com/spark/geoquiz/QuizActivity.java
@@ -9,17 +9,22 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageButton;
 import android.widget.TextView;
 import android.widget.Toast;
+
+import java.util.ArrayList;
 
 public class QuizActivity extends AppCompatActivity {
     private static final String TAG = "QuizActivity";
     private static final String KEY_INDEX = "index";
     private static final int REQUEST_CODE_CHEAT = 0;
+    private static final String KEY_CHEATED_QUESTIONS = "cheated_questions";
 
     private Button mTrueButton;
     private Button mFalseButton;
-    private Button mNextButton;
+    private ImageButton mPrevButton;
+    private ImageButton mNextButton;
     private Button mCheatButton;
     private TextView mQuestionTextView;
 
@@ -34,6 +39,12 @@ public class QuizActivity extends AppCompatActivity {
     private int mCurrentIndex = 0;
     private boolean mIsCheater;
 
+    private ArrayList<Integer> mCheatedQuestions = new ArrayList<>();
+
+    private boolean isCheater() {
+        return mCheatedQuestions.contains(mCurrentIndex);
+    }
+
     private void updateQuestion(){
         int question = mQuestionBank[mCurrentIndex].getTextResId();
         mQuestionTextView.setText(question);
@@ -44,7 +55,7 @@ public class QuizActivity extends AppCompatActivity {
 
         int messageResId = 0;
 
-        if(mIsCheater){
+        if(mIsCheater || isCheater()){
             messageResId = R.string.judgment_toast;
         }else {
             if (userPressedTrue == answerIsTrue) {
@@ -81,11 +92,29 @@ public class QuizActivity extends AppCompatActivity {
             }
         });
 
-        mNextButton = (Button) findViewById(R.id.next_button);
+        mPrevButton = (ImageButton) findViewById(R.id.prev_button);
+        mPrevButton.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                if(mCurrentIndex == 0) {
+                    mCurrentIndex = mQuestionBank.length - 1;
+                } else {
+                    mCurrentIndex--;
+                }
+                mIsCheater = false;
+                updateQuestion();
+            }
+        });
+
+        mNextButton = (ImageButton) findViewById(R.id.next_button);
         mNextButton.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View v) {
-                mCurrentIndex = (mCurrentIndex + 1) % mQuestionBank.length;
+                if(mCurrentIndex == mQuestionBank.length - 1) {
+                    mCurrentIndex = 0;
+                } else {
+                    mCurrentIndex++;
+                }
                 mIsCheater = false;
                 updateQuestion();
             }
@@ -103,6 +132,7 @@ public class QuizActivity extends AppCompatActivity {
 
         if(savedInstanceState != null){
             mCurrentIndex = savedInstanceState.getInt(KEY_INDEX, 0);
+            mCheatedQuestions = savedInstanceState.getIntegerArrayList(KEY_CHEATED_QUESTIONS);
         }
 
         updateQuestion();
@@ -118,6 +148,9 @@ public class QuizActivity extends AppCompatActivity {
                 return;
             }
             mIsCheater = CheatActivity.wasAnswerShown(data);
+            if (mIsCheater && ! isCheater()) {
+                mCheatedQuestions.add(mCurrentIndex);
+            }
         }
     }
 
@@ -126,6 +159,7 @@ public class QuizActivity extends AppCompatActivity {
         super.onSaveInstanceState(savedInstanceState);
         Log.i(TAG, "onSaveInstanceState");
         savedInstanceState.putInt(KEY_INDEX, mCurrentIndex);
+        savedInstanceState.putIntegerArrayList(KEY_CHEATED_QUESTIONS, mCheatedQuestions);
     }
 
     @Override
